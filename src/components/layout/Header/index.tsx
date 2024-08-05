b0a135c7fa6f070b89a6166f1fb44f839253cc4e
rin00000 <rinjun@naver.com>
2024-08-06 03:42:27 +0900

    feat: 헤더 수정 및 버튼 디자인
    - "영화 투표"와 "영화 퀴즈" 버튼을 왼쪽 정렬하고, "로그인" 버튼과 "mypage" 로고를 오른쪽 정렬하도록 헤더 구조를 수정했습니다.
    - 버튼의 다양한 변형과 크기를 위한 스타일을 정의하고 적용했습니다.
    - 작은 화면에서도 잘 보이도록 반응형 디자인을 적용했습니다.
    
    버튼 컴포넌트 분리 전입니다.

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -3,58 +3,115 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import colors from '../../../constants/colors';
 import { Button } from '../../common/Button';
+import mainLogo from '../../../assets/images/mainLogo.png';
+import mypageLogo from '../../../assets/images/mypageLogo.png';
 
-export default function Header() {
-  return (
-    <StyledHeaderContainer>
-      <StyledHeader>
-        <Link to="/">Mosdaq</Link>
-        <StyledNav>
-          <Button variant="secondary" size="small">
-            <Link to="/movie-list">영화 목록</Link>
-          </Button>
-          <Button variant="secondary" size="small">
-            <Link to="/quiz">퀴즈</Link>
-          </Button>
-          {/* <Link to="/mypage">마이페이지</Link> */}
-          <StyledLoginButton variant="secondary" size="small">
-            <Link to="/login">로그인</Link>
-          </StyledLoginButton>
-        </StyledNav>
-      </StyledHeader>
-    </StyledHeaderContainer>
-  );
-}
+const BaseButton = styled(Button).attrs(() => ({
+  variant: 'secondary',
+  size: 'small',
+}))`
+  text-decoration: none;
+  color: inherit;
+  display: inline-flex;
+  align-items: center;
+  padding: 0.5rem 1rem;
+  border-radius: 4px;
+  font-size: 0.875rem;
+  font-weight: 700;
+`;
 
-const StyledHeaderContainer = styled.section`
-  width: 100vw;
+const NavButton = styled(BaseButton)`
+  background-color: ${colors.white};
+  }
+`;
+
+const LoginButton = styled(BaseButton)`
+  background-color: ${colors.watcha};
+  color: ${colors.white};
+  border-radius: 7px;
+
+  &:hover {
+    background-color: ${colors.greyscale8};
+  }
+`;
+
+const Header = () => (
+  <HeaderContainer>
+    <HeaderContent>
+      <LeftSection>
+        <Link to="/">
+          <MainLogo src={mainLogo} alt="Main Logo" />
+        </Link>
+        <Nav>
+          <NavButton as={Link} to="/movie-list">
+            영화 투표
+          </NavButton>
+          <NavButton as={Link} to="/quiz">
+            영화 퀴즈
+          </NavButton>
+        </Nav>
+      </LeftSection>
+      <RightSection>
+        <LoginButton as={Link} to="/login">
+          로그인
+        </LoginButton>
+        <Link to="/mypage">
+          <MypageLogo src={mypageLogo} alt="MyPage Logo" />
+        </Link>
+      </RightSection>
+    </HeaderContent>
+  </HeaderContainer>
+);
+
+const HeaderContainer = styled.section`
+  width: 100%;
+  padding: 0.25rem 1rem;
   display: flex;
   justify-content: center;
   align-items: center;
-  border-bottom: 1px solid ${colors.grey300};
+  border-bottom: 1px solid ${colors.greyscale8};
+
+  @media (max-width: 768px) {
+    padding: 0.5rem;
+  }
 `;
 
-const StyledHeader = styled.header`
-  max-width: 960px;
-  height: 60px;
-  width: 100%;
+const HeaderContent = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  width: 100%;
+  max-width: 1200px;
+  height: 50px;
 
-  /* padding: 1rem; */
+  @media (max-width: 768px) {
+    height: auto;
+  }
 `;
 
-const StyledNav = styled.nav`
+const LeftSection = styled.div`
   display: flex;
+  align-items: center;
+`;
+
+const RightSection = styled.div`
+  display: flex;
+  align-items: center;
   gap: 1rem;
 `;
 
-const StyledLoginButton = styled(Button)`
-  background-color: crimson;
-  font-weight: 700;
-  color: ${colors.white};
-  &:hover {
-    background-color: red;
-  }
+const Nav = styled.nav`
+  display: flex;
+  gap: 0.5rem;
+  margin-left: 0.5rem;
 `;
+
+const MainLogo = styled.img`
+  width: 130px;
+`;
+
+const MypageLogo = styled.img`
+  width: 40x;
+`;
+
+export default Header;
