706cb9b070135fc6d25c1fc4c0690063b13d5200
rin00000 <rinjun@naver.com>
2024-08-07 12:25:29 +0900

    feat: 디자인 픽스된 컬러 팔레트 추가
    - greyscale11가지, watcha 3가지 색 추가했습니다.
    
    사용하지 않는 컬러들은 삭제 예정입니다.

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -4,8 +4,23 @@ const inverseGrey50 = '#202027';
 const inverseGrey100 = '#2c2c35';
 
 const colors = {
+  greyscale1: '#F1F1F1',
+  greyscale2: '#EAEAEA',
+  greyscale3: '#E5E5E5',
+  greyscale4: '#E0E0E0',
+  greyscale5: '#D9D9D9',
+  greyscale6: '#D0D0D0',
+  greyscale7: '#C7C7C7',
   greyscale8: '#B1B1B1',
+  greyscale9: '#A0A0A0',
+  greyscale10: '#8C8C8C',
+  greyscale11: '#6D6D6D',
+
   watcha: '#FF0558',
+  watcha2: '#FF73A1',
+  watcha3: '#FFBFD4',
+
+  // 삭제 예정 컬러들
   grey50: '#f9fafb',
   grey100,
   grey200: '#e5e8eb',
