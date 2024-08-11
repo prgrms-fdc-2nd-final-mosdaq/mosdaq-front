298a1408d62382918794c0182d68c591a6163964
tbs01215 <tbs01215@gmail.com>
2024-08-11 17:12:20 +0900

    feat: TYPOGRAPH_VARIANT 추가

--- src/components/common/Txt.tsx
+++ src/components/common/Txt.tsx
@@ -16,8 +16,36 @@ export function Txt({ typography = 'p', color = 'black', ...props }: Props) {
   );
 }
 
+type Typography =
+  | 'h1'
+  | 'h2'
+  | 'h3'
+  | 'p'
+  | 'Pretendard48bold'
+  | 'Pretendard40bold'
+  | 'Pretendard36bold'
+  | 'Pretendard32bold'
+  | 'Pretendard28bold'
+  | 'Pretendard24bold'
+  | 'Pretendard20bold'
+  | 'Pretendard18bold'
+  | 'Pretendard16bold'
+  | 'Pretendard14bold'
+  | 'Pretendard10bold'
+  | 'Pretendard48regular'
+  | 'Pretendard40regular'
+  | 'Pretendard36regular'
+  | 'Pretendard32regular'
+  | 'Pretendard28regular'
+  | 'Pretendard24regular'
+  | 'Pretendard20regular'
+  | 'Pretendard18regular'
+  | 'Pretendard16regular'
+  | 'Pretendard14regular'
+  | 'Pretendard10regular';
+
 const StyledText = styled.span<{
-  typography: 'h1' | 'h2' | 'h3' | 'p';
+  typography: Typography;
   color: string;
 }>`
   margin: 0;
@@ -43,6 +71,94 @@ const TYPOGRAPH_VARIANT = {
   `,
   p: css`
     font-size: 24px;
+    font-weight: 400;
+  `,
+  Pretendard48bold: css`
+    font-size: 48px;
+    font-weight: 700;
+  `,
+  Pretendard40bold: css`
+    font-size: 40px;
+    font-weight: 700;
+  `,
+  Pretendard36bold: css`
+    font-size: 36px;
+    font-weight: 700;
+  `,
+  Pretendard32bold: css`
+    font-size: 32px;
+    font-weight: 700;
+  `,
+  Pretendard28bold: css`
+    font-size: 28px;
+    font-weight: 700;
+  `,
+  Pretendard24bold: css`
+    font-size: 24px;
+    font-weight: 700;
+  `,
+  Pretendard20bold: css`
+    font-size: 20px;
+    font-weight: 700;
+  `,
+  Pretendard18bold: css`
+    font-size: 18px;
+    font-weight: 700;
+  `,
+  Pretendard16bold: css`
+    font-size: 16px;
+    font-weight: 700;
+  `,
+  Pretendard14bold: css`
+    font-size: 14px;
+    font-weight: 700;
+  `,
+  Pretendard10bold: css`
+    font-size: 10px;
+    font-weight: 700;
+  `,
+  Pretendard48regular: css`
+    font-size: 48px;
+    font-weight: 400;
+  `,
+  Pretendard40regular: css`
+    font-size: 40px;
+    font-weight: 400;
+  `,
+  Pretendard36regular: css`
+    font-size: 36px;
+    font-weight: 400;
+  `,
+  Pretendard32regular: css`
+    font-size: 32px;
+    font-weight: 400;
+  `,
+  Pretendard28regular: css`
+    font-size: 28px;
+    font-weight: 400;
+  `,
+  Pretendard24regular: css`
+    font-size: 24px;
+    font-weight: 400;
+  `,
+  Pretendard20regular: css`
+    font-size: 20px;
+    font-weight: 400;
+  `,
+  Pretendard18regular: css`
+    font-size: 18px;
+    font-weight: 400;
+  `,
+  Pretendard16regular: css`
+    font-size: 16px;
+    font-weight: 400;
+  `,
+  Pretendard14regular: css`
+    font-size: 14px;
+    font-weight: 400;
+  `,
+  Pretendard10regular: css`
+    font-size: 10px;
     font-weight: 400;
   `,
 };
