ae015c8c9f3db81e748defa62a319a9046c0f3ce
tbs01215 <tbs01215@gmail.com>
2024-08-23 18:15:55 +0900

    fix: 퀴즈 컴포넌트 수정

--- src/components/quiz/QuizContainer/QuizContents/QuizEnded.tsx
+++ src/components/quiz/QuizContainer/QuizContents/QuizEnded.tsx
@@ -1,5 +1,8 @@
 import { Button } from '@/components/common/Button';
+import { Txt } from '@/components/common/Txt';
+import colors from '@/constants/colors';
 import React from 'react';
+import styled from 'styled-components';
 
 interface IQuizEndedProps {
   correctCount: number;
@@ -12,16 +15,61 @@ export default function QuizEnded({
   correctCount,
   setQuizStatus,
 }: IQuizEndedProps) {
+  const getResultMessage = () => {
+    if (correctCount === 10) {
+      return `${correctCount}문제 맞추셨습니다. 완벽합니다!`;
+    } else if (correctCount >= 7) {
+      return `${correctCount}문제 맞추셨습니다. 많은 문제를 맞추셨네요!`;
+    } else if (correctCount >= 4) {
+      return `${correctCount}문제 맞추셨습니다. 나쁘지 않군요~`;
+    } else if (correctCount > 0) {
+      return `${correctCount}문제 맞추셨습니다. 더 잘할 수 있을 거에요!`;
+    } else {
+      return `${correctCount}문제 맞추셨습니다. 다시 도전해보세요!`;
+    }
+  };
+
   return (
-    <div>
-      <div>{correctCount}문제 맞추셨습니다. 짝짝짞 우와우와우와</div>
-      <Button
+    <StyledQuizEnded>
+      <StyledLandingDescription>
+        <Txt typography="Pretendard40bold">{getResultMessage()}</Txt>
+      </StyledLandingDescription>
+      <StyledButton
         onClick={() => {
           setQuizStatus('doing');
         }}
       >
-        다시 풀기
-      </Button>
-    </div>
+        <Txt color="white" typography="Pretendard32regular">
+          다시 풀기
+        </Txt>
+      </StyledButton>
+    </StyledQuizEnded>
   );
 }
+const StyledQuizEnded = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: ${colors.watcha};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const StyledLandingDescription = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: ${colors.white};
+  border-radius: 40px;
+  padding: 50px;
+`;
+const StyledButton = styled(Button)`
+  position: absolute;
+  bottom: 100px;
+  background-color: inherit;
+  border: 2px solid ${colors.white};
+  border-radius: 20px;
+
+  &:hover {
+    background-color: ${colors.watcha2};
+  }
+`;
