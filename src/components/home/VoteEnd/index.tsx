a62b537dc19ab785222ee5cd7edb5c1939fa61c0
jihwan1211 <jhwank1211@gmail.com>
2024-08-25 15:32:49 +0900

    feat:투표 마감 영화 컴포넌트 setting

---
+++ src/components/home/VoteEnd/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Txt } from '@/components/common/Txt';
+
+export default function VoteEnd() {
+  return (
+    <StyledWrapper>
+      <Txt typography="Pretendard48bold">투표 마감 영화</Txt>
+    </StyledWrapper>
+  );
+}
+
+const StyledWrapper = styled.div``;
