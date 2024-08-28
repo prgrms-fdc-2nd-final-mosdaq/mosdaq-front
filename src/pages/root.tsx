1056e03d22518ef29050d68946f761eb4319b0d3
tbs01215 <tbs01215@gmail.com>
2024-08-28 18:07:32 +0900

    feat: footer 구현

--- src/pages/root.tsx
+++ src/pages/root.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 // import styled from 'styled-components';
 import { Outlet } from 'react-router-dom';
 import Header from '../components/layout/Header';
+import Footer from '@/components/layout/Footer';
 
 interface IRootLayoutProps {
   preloadQuizPage: () => void;
@@ -17,6 +18,7 @@ export default function RootLayout(props: IRootLayoutProps) {
       {/* <StyledContentContainer> */}
       <Outlet />
       {/* </StyledContentContainer> */}
+      <Footer />
     </>
   );
 }
