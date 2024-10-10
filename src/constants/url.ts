f294fc962c5487c9c7f8377e155afdda0d66bbde
shgusgh12 <https://github.com/shgusgh12>
2024-10-10 16:00:48 +0900

    fix: URL 주소 변경

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,6 +1,6 @@
 // dev 서버 주소
 // export const BASE_URL = 'http://localhost:3000/api/v1';
-export const BASE_URL = 'http://mosdaq.site:3000/api/v1';
+export const BASE_URL = 'https://api.mosdaq.site';
 // export const BASE_URL = 'https://mosdaq.site:3000/api/v1';
 
 // console.log(BASE_URL);
