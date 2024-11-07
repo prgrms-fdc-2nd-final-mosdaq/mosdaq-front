5cee097e5f2431e22777ce51d529bfdd5f7ab65e
jihwan1211 <jhwank1211@gmail.com>
2024-11-07 21:45:09 +0900

    fix:quiz refetch

--- src/apis/quiz.api.ts
+++ src/apis/quiz.api.ts
@@ -2,7 +2,7 @@ import { IQuizResponse } from '@/models/quiz.model';
 import axiosInstance from './axiosInstance';
 
 export const fetchGetMovieQuiz = async () => {
-  const res = await axiosInstance.get<IQuizResponse>('movie/quiz');
+  const res = await axiosInstance.get<IQuizResponse>('movie/quiz?count=10');
 
   return res.data;
 };
