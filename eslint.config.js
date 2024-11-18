3e35c0c25b53111d412df1108353d2cc3cf5e214
javascriptbiggosoo <tbs01215@gmail.com>
2024-11-18 17:52:33 +0900

    feat: 린트 룰 완화

--- eslint.config.js
+++ eslint.config.js
@@ -5,9 +5,19 @@ import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'no-unexpected-multiline': 'warn',
+      'react/no-unescaped-entities': 'off',
+      'react/display-name': 'off',
+    },
+  },
 ];
